cb2663ec8a2ef32ab9901e3fbff699a8a1d3045f
Jaider <jaidercueto.1010@gmail.com>
2023-06-13 10:52:47 -0500

    global styles modified. Link styles

--- frontend-next/src/app/components/navBar/NavBar.jsx
+++ frontend-next/src/app/components/navBar/NavBar.jsx
@@ -5,10 +5,10 @@ import styles from './NavBar.module.css'
 export default function NavBar() {
   return (
     <header className={styles.container}>
-      <div>Raffles App</div>
+      <Link href='/'><div>Raffles App</div></Link>
       <ul>
-        <li>Jaider</li>
-        <li>Nueva Rifa</li>
+        <Link href='#'><li>Jaider</li></Link>
+        <Link href='/createRaffle'><li>Nueva Rifa</li></Link>
       </ul>
     </header>
   )
