a914f9bc4f507c366b5d040e19aefae37a1b08fd
Jaider <jaidercueto.1010@gmail.com>
2023-06-20 12:51:15 -0500

    Controlled form using localState. Global one modified after submitting. Styles applied

---
+++ frontend-next/src/app/createRaffle/initialState.js
@@ -0,0 +1,11 @@
+
+const id = -1
+
+export const initialState = {
+    id,
+    numTickets: 50,
+    prize: '',
+    lotery: '',
+    date: '',
+    price: 0
+}
--- frontend-next/src/app/createRaffle/page.jsx
+++ frontend-next/src/app/createRaffle/page.jsx
@@ -1,36 +1,67 @@
+'use client'
+
 import React from 'react'
 import styles from './createRaffle.module.css'
 import NavBar from '../components/navBar/NavBar'
+import { useThemeContext } from '@/context/raffleState'
+import { useState } from 'react'
+import { initialState } from './initialState'
 
 export default function CreateRaffle() {
+
+  const [raffleGlobal, setRaffleGlobal] = useThemeContext()
+
+  const [raffleInfo, setRaffleInfo] = useState(initialState)
+
+  const handleChange = (e) => {
+    const {value, name} = e.target
+        setRaffleInfo(prevState => (
+            {
+                ...prevState,
+                [name]: value
+            }
+        ))
+    }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    setRaffleGlobal(prevState => {
+        return {
+            ...prevState,
+            rafflesArray: [...prevState.rafflesArray, {id: raffleInfo.id++, ...raffleInfo}]
+        }
+    })
+    setRaffleInfo(initialState)
+  }
+
   return (
    <>
    <NavBar/>
     <div className={styles.container}>
-        <form>
+        <form onSubmit={handleSubmit}>
             <label>
                 Seleccione cuantas boletas tendrá la rifa
-                <select>
-                    <option>50 boletas</option>
-                    <option>100 boletas</option>
-                    <option>200 boletas</option>
+                <select name='numTickets' onChange={handleChange}>
+                    <option name='numTickets' value={50}>50 boletas</option>
+                    <option name='numTickets' value={100}>100 boletas</option>
+                    <option name='numTickets' value={200}>200 boletas</option>
                 </select>
             </label>
             <label>
                 Ingrese el articulo a rifar
-                <input type='text'/>
+                <input onChange={handleChange} name='prize' value={raffleInfo.prize} type='text'/>
             </label>
             <label>
                 Indique con qué lotería se sabrá el ganador
-                <input type='text'/>
+                <input onChange={handleChange} name='lotery' value={raffleInfo.lotery} type='text'/>
             </label>
             <label>
                 Indique la fecha en que jugará la rifa
-                <input type="date" />
+                <input onChange={handleChange} name='date' value={raffleInfo.date} type="date" />
             </label>
             <label>
                 Establezca el precio de la boleta (COP)
-                <input type="number" step='500'/>
+                <input onChange={handleChange} name='price' value={raffleInfo.price} type="number" step='500'/>
             </label>
             <input type="submit" value="Crear Rifa"/>
         </form>
