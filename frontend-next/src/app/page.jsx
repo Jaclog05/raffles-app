9717a1cde49b97f72a2fe68f5241ad9deb8dd751
Jaider <jaidercueto.1010@gmail.com>
2023-06-20 12:46:23 -0500

    Wrapping app with ThemeProvider. Using global state in HomePage

--- frontend-next/src/app/page.jsx
+++ frontend-next/src/app/page.jsx
@@ -1,12 +1,31 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+'use client'
+
 import NavBar from './components/navBar/NavBar'
+import { useThemeContext } from '@/context/raffleState'
+import Raffle from './components/raffleComponent/Raffle'
 
 export default function Home() {
+
+  const [raffleInfo, setRaffleInfo] = useThemeContext()
+
+
   return (
     <>
       <NavBar/>
-      Home Page
+      {
+        raffleInfo.rafflesArray.map((raffle, idx) => {
+          return (
+            <Raffle
+                key={idx}
+                numTickets={raffle.numTickets}
+                prize={raffle.prize}
+                lotery={raffle.lotery}
+                date={raffle.date}
+                price={raffle.price}
+            />
+          )
+        })
+      }
     </>
   )
 }
