4ab2ef4b9fb85f2987fa92db260e8c27a06227b9
Jaider <jaidercueto.1010@gmail.com>
2023-06-20 12:49:07 -0500

    Receiving and rendering data in RaffleComponent. Basic styles

--- frontend-next/src/app/components/raffleComponent/Raffle.jsx
+++ frontend-next/src/app/components/raffleComponent/Raffle.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
 import styles from './Raffle.module.css'
 
-export default function Raffle() {
+export default function Raffle({numTickets, prize, lotery, date, price}) {
   return (
-    <div>Raffle Component</div>
+    <div className={styles.container}>
+      <h4>{numTickets}</h4>
+      <h4>{prize}</h4>
+      <h4>{lotery}</h4>
+      <h4>{date}</h4>
+      <h4>{price}</h4>
+    </div>
   )
 }
