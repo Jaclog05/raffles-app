200add33f5480b9f48ff33fb9aa67b4fdbb38cad
Jaider <jaidercueto.1010@gmail.com>
2023-06-29 19:10:06 -0500

    Changing hard-coded data for state data. No raffle found error styles

--- frontend-next/src/app/raffle/[id]/page.jsx
+++ frontend-next/src/app/raffle/[id]/page.jsx
@@ -1,24 +1,44 @@
+'use client'
+
 import React from 'react'
 import styles from './raffleDetails.module.css'
+import { useThemeContext } from '@/context/raffleState'
+import { useState } from 'react'
+import daysToRaffle from '@/app/components/raffleComponent/raffleDates'
 
 export default function RaffleDetails({params}) {
+
+  const [raffleInfo, setRaffleInfo] = useThemeContext()
+
+  const filterRaffle = (rafflesArray, id) => {
+    return rafflesArray.find(raffle => raffle.id == id)
+  }
+
+  const today = new Date()
+
+  const [raffle, setRaffle] = useState(filterRaffle(raffleInfo.rafflesArray, params.id))
+
   return (
-    <div className={styles.wrapper}>
-      <div className={styles.container}>
-        <div className={styles.left}>
-            <h2>Televisor</h2> 
-            <img src='https://pc-tecnologia.com/855-large_default/televisor-samsung-32-pulgadas-serie-4.jpg' alt='prize'/>
+    <div className={ raffle ? styles.wrapper : styles.raffleNotFound}>
+      { raffle  ? 
+        <div className={styles.container}>
+          <div className={styles.left}>
+              <h2>{raffle.prize}</h2> 
+              <img src='https://pc-tecnologia.com/855-large_default/televisor-samsung-32-pulgadas-serie-4.jpg' alt='prize'/>
+          </div>
+          <div className={styles.right_up}>
+            board Component
+          </div>
+          <div className={styles.right_bottom}>
+              <h2>{raffle.numTickets} Boletas</h2>
+              <h4>Juega con la lotería {raffle.lotery}</h4>
+              <h3>{daysToRaffle(today, raffle.date)}</h3>
+              <button>Comprar boletas</button>
+          </div>
         </div>
-        <div className={styles.right_up}>
-          board Component
-        </div>
-        <div className={styles.right_bottom}>
-            <h2>100 boletas</h2>
-            <h4>La Caribeña</h4>
-            <h3>30 de Junio, 2023</h3>
-            <button>Comprar boletas</button>
-        </div>
-      </div>
+        : 
+          <h2>Rifa No Encontrada 🤔</h2>
+      }
     </div>
   )
 }
