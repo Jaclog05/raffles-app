ebf423af1332e53d7764a0837346165a93245b88
Jaider <jaidercueto.1010@gmail.com>
2023-07-14 16:49:43 -0500

    Adding local state to Board Component. Styles picking numbers

--- frontend-next/src/app/components/board/Board.jsx
+++ frontend-next/src/app/components/board/Board.jsx
@@ -1,15 +1,38 @@
-import React from 'react'
+import React, {useState} from 'react'
 import styles from './Board.module.css'
 
 export default function Board({numTickets}) {
 
-  const elements = Array.from({ length: parseInt(numTickets) }, (_, index) => index + 1);
+  const elements = Array.from({ length: parseInt(numTickets) }, (_, index) => {
+    return {
+      value: index + 1,
+      picked: false
+    }
+  });
+
+  const [boardState, setBoardState] = useState(elements)
+
+  const toggle = (index) => {
+    setBoardState((prevItems) => {
+      const updatedItems = [...prevItems];
+      const item = { ...updatedItems[index] };
+      item.picked = !item.picked; 
+      updatedItems[index] = item;
+      return updatedItems;
+    });
+  };
 
   return (
     <div className={styles.wrapper}>
         {
-            elements.map(number => (
-                <p key={number}>{number}</p>
+            boardState.map((number, idx) => (
+                <p 
+                  key={number.value}
+                  onClick={() => toggle(idx)}
+                  className={number.picked ? styles.picked : styles.noPicked}
+                >
+                  {number.value}
+                </p>
             ))
         }
     </div>
